e8bd81f681c23dd8a072f204f6ca82176c921de2
Zeyu Li <go72wog@mytum.de>
2025-06-22 18:39:14 +0200

    add monitor

--- genai/app/main.py
+++ genai/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.api import router
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI(
     title="LLM Suggestion Service",
@@ -7,4 +9,6 @@ app = FastAPI(
     version="1.0.0"
 )
 
-app.include_router(router) 
+Instrumentator().instrument(app).expose(app, include_in_schema=False, endpoint="/metrics")
+
+app.include_router(router, prefix="/genai") 
