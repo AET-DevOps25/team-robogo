fb8064588f0b93c642b26bf7c3d36dea30edd1f6
Lucas Welscher <ga53foy@mytum.de>
2025-07-20 17:58:03 +0200

    CD: Read and use AWS DNS

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,3 +2,7 @@ output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.robogo_ec2.public_ip
 }
+
+output "ec2_public_dns" {
+  value = aws_instance.robogo_ec2.public_dns
+}
